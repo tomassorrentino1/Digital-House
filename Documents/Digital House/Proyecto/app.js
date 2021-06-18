bcf9e31466edb0ac02c6f8743531ac03b93fad2c
Tomas Sorrentino <sorrentinotomas1@gmail.com>
2021-06-18 00:08:47 -0300

    commit para heroku

--- Documents/Digital House/Proyecto/app.js
+++ Documents/Digital House/Proyecto/app.js
@@ -6,9 +6,10 @@ const app = express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('servidor corriendo')
-});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('servidor corriendo en el puerto 3000');
+})
 
 app.get('/', (req, res) =>{
     const ruta = path.resolve(__dirname, 'views/home.html');
